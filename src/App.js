5af6a29587619e9e7efc345de50b3b0ca9efd3b2
Katrina Aganon <katrina.aganon@gmail.com>
2018-12-09 18:27:28 -0800

    add onChangeCallback in Radioset component

--- src/App.js
+++ src/App.js
@@ -7,9 +7,24 @@ import songData from './data/tracks.json';
 
 songData.forEach((song, i) => {
   song.id = i;
+  song.favorite = false;
 });
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      allSongs: songData,
+    };
+  };
+
+  onStarChange = (id) => {
+    let favoriteChange = this.state.allSongs;
+    favoriteChange[id].favorite = !favoriteChange[id].favorite;
+    this.setState({allSongs: favoriteChange});
+  };
+
+
   render() {
     return (
       <div className="App">
@@ -17,7 +32,7 @@ class App extends Component {
           <h1 className="page-header--title">Radio Lovelace</h1>
         </header>
         <main className="main">
-          <RadioSet tracks={songData} />
+          <RadioSet tracks={songData} onChangeCallback={this.onStarChange}/>
         </main>
       </div>
     );
