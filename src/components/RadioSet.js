4ecc7455ccceb48656a4716c34d26cee9bd1beea
Katrina Aganon <katrina.aganon@gmail.com>
2018-12-09 18:26:52 -0800

    add onItemChange callback in Playlist components

--- src/components/RadioSet.js
+++ src/components/RadioSet.js
@@ -9,16 +9,24 @@ const RadioSet = (props) => {
     morningTracks: props.tracks.slice(0, props.tracks.length / 2),
     eveningTracks: props.tracks.slice(props.tracks.length / 2, props.tracks.length)
   };
+
+  const onChangeCallback = (id) => {
+    let trackId = id;
+    this.props.onStarChangeCallback(trackId);
+  };
+
   return (
     <div className="radio-set">
       <section className="radio-set--playlist-container">
         <Playlist
           side="Morning"
           tracks={playlists.morningTracks}
+          onItemChange={props.onChangeCallback}
         />
         <Playlist
           side="Evening"
           tracks={playlists.eveningTracks}
+          onItemChange={props.onChangeCallback}
         />
       </section>
     </div>
